d9f957b66ca479ea842e36277f7e2e80af4ba135
LilTaiga <liltaigaishappy@gmail.com>
2022-07-07 13:07:26 -0300

    Changed members name

--- thing.cpp
+++ thing.cpp
@@ -2,25 +2,25 @@
 
 double Thing::get_x_pos()
 {
-    return m_x_pos;
+    return x_pos;
 }
 
 double Thing::get_y_pos()
 {
-    return m_y_pos;
+    return y_pos;
 }
 
 double Thing::get_rot()
 {
-    return m_rot;
+    return rot;
 }
 
 double Thing::get_x_scale()
 {
-    return m_x_scale;
+    return x_scale;
 }
 
 double Thing::get_y_scale()
 {
-    return m_y_scale;
+    return y_scale;
 }
