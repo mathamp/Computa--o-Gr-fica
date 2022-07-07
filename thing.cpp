2c40814b81b6f02d3c0dc8da391c122ff233198b
LilTaiga <liltaigaishappy@gmail.com>
2022-07-07 20:50:33 -0300

    Implemented Thing class with Transform2

--- thing.cpp
+++ thing.cpp
@@ -1,4 +1,7 @@
 #include "thing.hpp"
+#include "utils/transform2.hpp"
+
+using Transform::Transform2;
 
 double Thing::get_x_pos()
 {
@@ -23,4 +26,44 @@ double Thing::get_x_scale()
 double Thing::get_y_scale()
 {
     return y_scale;
+}
+
+
+
+void Thing::translate(double dx, double dy)
+{
+    x_pos += dx;
+    y_pos += dy;
+
+    Transform2 t = Transform::get_translation_matrix(dx, dy);
+    apply_matrix(t);
+}
+
+void Thing::scale(double sx, double sy)
+{
+    x_scale *= sx;
+    y_scale *= sy;
+
+    Transform2 t = Transform::get_scale_matrix(sx, sy);
+    apply_matrix(t);
+}
+
+void Thing::rotate_radians(double amount)
+{
+    rot += amount;
+
+    Transform2 t = Transform::get_rotation_matrix(amount);
+    apply_matrix(t);
+}
+
+void Thing::rotate_degrees(double amount)
+{
+    rotate_radians(amount * 3.14159265358979323846 / 180.0);
+}
+
+
+
+void Thing::apply_matrix(Transform2 t)
+{
+    transform = transform.apply_matrix(t);
 }
