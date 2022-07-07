dbf1e8d4fae0b50d559086561de1d79e1db0bb9b
LilTaiga <liltaigaishappy@gmail.com>
2022-07-07 18:02:45 -0300

    Created prototype for Matrix3 class

---
+++ utils/matrix3.hpp
@@ -0,0 +1,28 @@
+namespace Matrix
+{
+	class Matrix3
+	{
+		private:
+			double m[3][3];
+
+		public:
+			Matrix3();
+			Matrix3(double values[3][3]);
+			Matrix3(const Matrix3& matrix);
+			
+			double get(int _m, int _n);
+			double set(int _m, int _n, double _value);
+
+			Matrix3 operator + (const Matrix3& other);
+			Matrix3 operator - (const Matrix3& other);
+
+			Matrix3 operator * (const Matrix3& other);
+			Matrix3 operator * (const Matrix3& other);
+	};
+
+	Matrix3 add(const Matrix3& a, const Matrix3& b);
+	Matrix3 sub(const Matrix3& a, const Matrix3& b);
+
+	Matrix3 mul(double a, const Matrix3& b);
+	Matrix3 mul(const Matrix3& a, const Matrix3& b);
+}
