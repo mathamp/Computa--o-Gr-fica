e5210847cb1378038346499b5f43b161a255f52d
LilTaiga <liltaigaishappy@gmail.com>
2022-07-07 13:06:37 -0300

    Changed style of code

--- thing.hpp
+++ thing.hpp
@@ -3,18 +3,18 @@
 class Thing
 {
     private:
-        double m_x_pos;
-        double m_y_pos;
+        double x_pos;
+        double y_pos;
 
-        double m_rot;
+        double rot;
 
-        double m_x_scale;
-        double m_y_scale;
+        double x_scale;
+        double y_scale;
 
         double matrix[3][3];
 
     public:
-        Thing(double x, double y, double rot);
+        Thing(double _x, double _y, double _rot);
 
         double get_x_pos();
         double get_y_pos();
@@ -24,16 +24,16 @@ class Thing
         double get_x_scale();
         double get_y_scale();
 
-        void translate(double dx, double dy);
-        void translate_matrix(double translation_matrix[3][3]);
+        void translate(double _dx, double _dy);
+        void translate_matrix(double _translation_matrix[3][3]);
 
-        void rotate(double amount);
-        void rotate_matrix(double rotation_matrix[3][3]);
+        void rotate(double _amount);
+        void rotate_matrix(double _rotation_matrix[3][3]);
 
-        void scale(double dx, double dy);
-        void scale_matrix(double scale_matrix[3][3]);
+        void scale(double _dx, double _dy);
+        void scale_matrix(double _scale_matrix[3][3]);
 
-        void apply_matrix(double transformation_matrix[3][3]);
+        void apply_matrix(double _transformation_matrix[3][3]);
 
         virtual void draw();
 };
