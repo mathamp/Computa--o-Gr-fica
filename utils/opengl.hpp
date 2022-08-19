7a9ee8198b12d23d986d4e00ee3f5b3543b084b7
LilTaiga <liltaigaishappy@gmail.com>
2022-08-19 12:32:28 -0300

    a

--- utils/opengl.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void init(int* argc, char** argv, unsigned int width, unsigned int height);
-void place_pixel(unsigned int x, unsigned int y);
-void place_pixel(unsigned int x, unsigned int y, float r, float g, float b);
